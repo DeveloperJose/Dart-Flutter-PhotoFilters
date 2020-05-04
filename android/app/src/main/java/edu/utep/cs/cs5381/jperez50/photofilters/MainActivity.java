c76aeebaf9963566b9d0b7315804032db2e32be3
DeveloperJose <josegperez@mail.com>
2020-05-04 17:43:55 -0600

    Add initial flutter project set-up

---
+++ android/app/src/main/java/edu/utep/cs/cs5381/jperez50/photofilters/MainActivity.java
@@ -0,0 +1,13 @@
+package edu.utep.cs.cs5381.jperez50.photofilters;
+
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity;
+import io.flutter.embedding.engine.FlutterEngine;
+import io.flutter.plugins.GeneratedPluginRegistrant;
+
+public class MainActivity extends FlutterActivity {
+  @Override
+  public void configureFlutterEngine(@NonNull FlutterEngine flutterEngine) {
+    GeneratedPluginRegistrant.registerWith(flutterEngine);
+  }
+}
